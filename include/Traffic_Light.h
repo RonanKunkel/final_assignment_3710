f7599c8cd0892f0ad6102803a2a2141c7cdabdcc
Ronan Kunkel <153593424+RonanKunkel@users.noreply.github.com>
2024-10-30 15:52:48 -0600

    Add files via upload

---
+++ include/Traffic_Light.h
@@ -0,0 +1,7 @@
+/**
+* @author Ronan Kunkel [r.kunkel@uleth.ca]
+* @author Desmond Clarke []
+* @author Maksym Makarov []
+* @author Matthew Gillett []
+* @date 2024-10
+*/
---
+++ src/project/main.cc
@@ -0,0 +1,20 @@
+/**
+* @author Ronan Kunkel [r.kunkel@uleth.ca]
+* @author Desmond Clarke []
+* @author Maksym Makarov []
+* @author Matthew Gillett []
+* @date 2024-10
+*/
+
+#include "Angel.h"
+#include "Buildings.h"
+#include "Camera.h"
+#include "Car.h"
+#include "City.h"
+#include "Traffic_Light.h"
+
+
+int main() {
+
+    return 0;
+}
