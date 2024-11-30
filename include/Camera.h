32484197e467a9597f2bba7561e01e91f7ae5365
DeltaC4 <clarkejdesmond@gmail.com>
2024-11-30 13:05:09 -0700

    .h comments added

--- include/Camera.h
+++ include/Camera.h
@@ -5,33 +5,31 @@
 
 using namespace std;
 
-class Camera
-{
+class Camera {
 public:
-  Camera(GLuint viewLoc, GLuint projLoc,
-	 vec4 eye, vec4 at, vec4 up,
-	 int oleft, int oright, int obot, int otop, int oznear,
-	 int ozfar);
-  ~Camera();
-
-  static Camera* createOrthoCamera(GLuint viewLoc, GLuint projLoc);
-  static Camera* createPerspectiveCamera(GLuint viewLoc, GLuint projLoc);
-
-
-  void moveEye(GLfloat dx, GLfloat dy, GLfloat dz);
-  void setView(vec4 eye, vec4 at, vec4 up);
-  void setProjection(mat4 projection);
-  void sendToShader() const;
-  
-
-  void setDefaultView(vec4 car_position);
-  void setOverheadView(vec4 car_position);
-  void setSideView(vec4 car_position);
-
+ // Constructor for camera
+ Camera(GLuint viewLoc, GLuint projLoc,
+  vec4 eye, vec4 at, vec4 up,
+  int oleft, int oright, int obot, int otop, int oznear,
+  int ozfar);
+ ~Camera(); 
+ // functions for creating the two different types of cameras
+ static Camera* createOrthoCamera(GLuint viewLoc, GLuint projLoc);
+ static Camera* createPerspectiveCamera(GLuint viewLoc, GLuint projLoc);
+ // eye fucntions for movement of eye
+ void moveEye(GLfloat dx, GLfloat dy, GLfloat dz);
+ void setView(vec4 eye, vec4 at, vec4 up);
+ void setProjection(mat4 projection);
+ void sendToShader() const;
+ // functions for creating the two different types of cameras
+ void setDefaultView(vec4 car_position);
+ void setOverheadView(vec4 car_position);
+ void setSideView(vec4 car_position); 
 private:
-  GLuint view_loc, proj_loc;
-  vec4 eye_pos, at_pos, up_dir;
-  mat4 view_mat, proj_mat;
+ // variables for creation and movement of cameras
+ GLuint view_loc, proj_loc;
+ vec4 eye_pos, at_pos, up_dir;
+ mat4 view_mat, proj_mat;
 };
 
 #endif
--- include/Plane.h
+++ include/Plane.h
@@ -6,21 +6,30 @@
 
 class Plane : public Object {
  public:
-    Plane(GLuint vertexloc, GLuint faceloc, GLuint modeloc,
-    vec4 pos, GLfloat theta_x = 0, GLfloat theta_y = 0,GLfloat theta_z = 0,
-    GLfloat scale_x = 1,GLfloat scale_y = 1,GLfloat scale_z = 1);
-    ~Plane();
-    void draw() const;
-    void moveLeft();
-    void moveRight();
-    void moveForward();
-    void moveBackward();
-    int getDirection();
-
-    vec4 currentPosition;
+  // Constructor for plane keeping scale to 1 for every vertice
+  Plane(GLuint vertexloc, GLuint faceloc, GLuint modeloc,
+  vec4 pos, GLfloat theta_x = 0, GLfloat theta_y = 0,GLfloat theta_z = 0,
+  GLfloat scale_x = 1,GLfloat scale_y = 1,GLfloat scale_z = 1);
+  ~Plane();
+  // fucntion to draw the plane
+  void draw() const;
+  // movement functions
+  //moveLeft: rotates object right, updates current direction.
+  //moveRight: rotates object left, updates current direction.
+  //moveForward translates object backwards depending on current direcion
+  //moveBackwards translates object forwards. depending on current direcion
+  void moveLeft();
+  void moveRight();
+  void moveForward();
+  void moveBackward();
+  // function used to restrict the players movement to the roads only
+  int getDirection();   
+  // vec4 also used to restrict players movement to roads only 
+  vec4 currentPosition;
  private:
- enum direction {north,east, south, west};
- direction currentdirection;
+  // variables used for creation and movment of the plane object
+  enum direction {north,east, south, west};
+  direction currentdirection;
   mat4 model;
   GLuint vertex_loc, face_loc, model_loc;
   GLuint vao[1], buffer[1];
